f9eeb18af595c6bdb79fc274a3cdf26eb34c7b82
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2025-05-22 11:14:25 -0500

    Changes in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-function add(a,b) {
-return a+b;
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a + b;
 }
 
-module.exports={add};
+function subtract(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a - b;
+}
+
+module.exports = {
+  add,
+  subtract
+};
+
